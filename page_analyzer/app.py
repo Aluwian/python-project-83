16fff3a0f94653811ad51093b5bd003872ef75d4
Anna Bystrova <annbystr@gmail.com>
2023-07-03 16:13:03 +0300

    refactor

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -7,7 +7,7 @@ from flask import (
     flash,
     get_flashed_messages
 )
-from page_analyzer.dbase import UrlsTable, Url_Checks
+from page_analyzer import dbase
 from page_analyzer.validator import validate_url
 from page_analyzer.analyzer import analyze_page
 import os
@@ -31,8 +31,7 @@ def get_404(error):
 
 @app.get('/urls')
 def get_urls_page():
-    table = UrlsTable()
-    array = table.get_all_info()
+    array = dbase.get_all_urls()
     return render_template('pages/urls/index.html',
                            urls=array
                            )
@@ -47,8 +46,7 @@ def create_new_url():
                                url=data,
                                messages=errors,
                                ), 422
-    url_base = UrlsTable()
-    values = url_base.has_name(data)
+    values = dbase.has_url_name(data.get('url'))
     id, result = values
     flash(result[1], result[0])
     return redirect(url_for('get_url', id=id))
@@ -56,11 +54,9 @@ def create_new_url():
 
 @app.get('/urls/<int:id>')
 def get_url(id):
-    urt_table = UrlsTable()
-    checks_base = Url_Checks()
-    data = urt_table.get_url_info(id)
+    data = dbase.get_one_url(id)
     messages = get_flashed_messages(with_categories=True)
-    checks = checks_base.get_all_checks(id)
+    checks = dbase.get_all_checks(id)
     return render_template('pages/urls/new.html',
                            id=id,
                            name=data[1],
@@ -71,12 +67,10 @@ def get_url(id):
 
 @app.post("/urls/<int:id>/checks")
 def get_check(id):
-    urls = UrlsTable()
-    link = urls.get_url_info(id)[1]
+    link = dbase.get_one_url(id)[1]
     try:
-        checks_base = Url_Checks()
         check_result = analyze_page(link)
-        result = checks_base.create_new(id, check_result)
+        result = dbase.add_check(id, check_result)
         flash(result[1], result[0])
         return redirect(url_for('get_url',
                                 id=id))
--- page_analyzer/dbase.py
+++ page_analyzer/dbase.py
@@ -2,139 +2,149 @@ import os
 from psycopg2 import connect, sql
 from datetime import date
 from page_analyzer.validator import normalize_url
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-def connect_to_db():
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    return connect(DATABASE_URL)
+def add_url(url_text):
+    with connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            created_ad = date.today()
+            cur.execute("INSERT INTO urls (name, created_at)"
+                        "VALUES (%s, %s) RETURNING id;", (url_text, created_ad))
+            result = cur.fetchone()
+    conn.commit()
+    conn.close()
+    return result[0]
 
 
-class UrlsTable:
-    def __init__(self):
-        self.connect = connect_to_db()
+def add_check(url_id, page_info):
+    with connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            created_at = date.today()
+            cur.execute("INSERT INTO url_checks ("
+                        "url_id, status_code, h1, title, description, created_at"
+                        ")"
+                        "VALUES (%s, %s, %s, %s, %s, %s);",
+                        (
+                            url_id,
+                            page_info.get('status_code'),
+                            page_info.get('h1'),
+                            page_info.get('title'),
+                            page_info.get('description'),
+                            created_at
+                        )
+                        )
+    conn.commit()
+    conn.close()
+    return 'success', 'Страница успешно проверена'
 
-    def fill_table(self, url_text):
-        cur = self.connect.cursor()
-        created_ad = date.today()
-        cur.execute("INSERT INTO urls (name, created_at)"
-                    "VALUES (%s, %s) RETURNING id;", (url_text, created_ad))
-        result = cur.fetchone()
-        self.connect.commit()
-        return result[0]
 
-    def has_name(self, dictionary):
-        url_name = normalize_url(dictionary.get('url'))
-        cur = self.connect.cursor()
-        query = sql.SQL("SELECT {find} FROM {table} WHERE {key} = %s").format(
-            find=sql.Identifier('id'),
-            table=sql.Identifier('urls'),
-            key=sql.Identifier('name')
-        )
-        cur.execute(query, (url_name,))
-        result = cur.fetchone()
-        if result is None:
-            return self.fill_table(url_name),\
-                ('success', 'Страница успешно добавлена')
-        return result[0], ('info', 'Страница уже существует')
-
-    def get_url_info(self, id):
-        cur = self.connect.cursor()
-        query = sql.SQL("SELECT * FROM {table} WHERE {key} = %s").format(
-            table=sql.Identifier('urls'),
-            key=sql.Identifier('id')
-        )
-        cur.execute(query, (id,))
-        return cur.fetchone()
-
-    def get_all_info(self):
-        cur = self.connect.cursor()
-        result = []
-        checks = Url_Checks()
-        cur.execute("SELECT id, name FROM urls ORDER BY id DESC;")
-        array = cur.fetchall()
-        for value in array:
-            result.append(
-                {
-                    'id': value[0],
-                    'name': value[1],
-                    'last_check': checks.get_last(value[0]).get('created_at'),
-                    'status_code': checks.get_last(value[0]).get('status_code')
-                }
+def has_url_name(url):
+    with connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            url_name = normalize_url(url)
+            query = sql.SQL("SELECT {find} FROM {table} WHERE {key} = %s").format(
+                find=sql.Identifier('id'),
+                table=sql.Identifier('urls'),
+                key=sql.Identifier('name')
             )
-        return result
+            cur.execute(query, (url_name,))
+            result = cur.fetchone()
+    conn.close()
+    if result is None:
+        return add_url(url_name), \
+            ('success', 'Страница успешно добавлена')
+    return result[0], ('info', 'Страница уже существует')
 
 
-class Url_Checks:
-    def __init__(self):
-        self.connect = connect_to_db()
+def get_one_url(url_id):
+    with connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            query = sql.SQL("SELECT * FROM {table} WHERE {key} = %s").format(
+                table=sql.Identifier('urls'),
+                key=sql.Identifier('id')
+            )
+            cur.execute(query, (url_id,))
+            result = cur.fetchone()
+    conn.close()
+    return result
 
-    def create_new(self, url_id, page_info):
-        cur = self.connect.cursor()
-        created_at = date.today()
-        cur.execute("INSERT INTO url_checks ("
-                    "url_id, status_code, h1, title, description, created_at"
-                    ")"
-                    "VALUES (%s, %s, %s, %s, %s, %s);",
-                    (
-                        url_id,
-                        page_info.get('status_code'),
-                        page_info.get('h1'),
-                        page_info.get('title'),
-                        page_info.get('description'),
-                        created_at
-                    )
-                    )
-        self.connect.commit()
-        return 'success', 'Страница успешно проверена'
 
-    def get_all_checks(self, url_id):
-        cur = self.connect.cursor()
-        result = []
-        query = sql.SQL(
-            "SELECT {id}, {status}, {h1}, {title}, {desc}, {date} FROM {table}"
-            "WHERE {value_3} = %s"
-            "ORDER BY {id} DESC;"
-        ).format(
-            id=sql.Identifier('id'),
-            status=sql.Identifier('status_code'),
-            h1=sql.Identifier('h1'),
-            title=sql.Identifier('title'),
-            desc=sql.Identifier('description'),
-            date=sql.Identifier('created_at'),
-            value_3=sql.Identifier('url_id'),
-            table=sql.Identifier('url_checks')
-        )
-        cur.execute(query, (url_id,))
-        array = cur.fetchall()
-        for value in array:
-            result.append({
-                'id': value[0],
-                'status_code': value[1],
-                'h1': value[2],
-                'title': value[3],
-                'description': value[4],
-                'created_at': value[5]
-            })
-        return result
-
-    def get_last(self, value_id):
-        cur = self.connect.cursor()
-        query = sql.SQL(
-            "SELECT {create_at}, {status_code} FROM {table}"
-            "WHERE {value_2} = %s "
-            "ORDER BY {value_3} DESC LIMIT 1;").format(
-            create_at=sql.Identifier('created_at'),
-            status_code=sql.Identifier('status_code'),
-            value_2=sql.Identifier('url_id'),
-            value_3=sql.Identifier('id'),
-            table=sql.Identifier('url_checks')
-        )
-        cur.execute(query, (value_id,))
-        result = cur.fetchone()
-        if result is None:
-            return {"created_at": "",
-                    "status_code": ""
+def get_all_urls():
+    with connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            result = []
+            cur.execute("SELECT id, name FROM urls ORDER BY id DESC;")
+            array = cur.fetchall()
+            for value in array:
+                result.append(
+                    {
+                        'id': value[0],
+                        'name': value[1],
+                        'last_check': get_last_check(value[0]).get('created_at'),
+                        'status_code': get_last_check(value[0]).get('status_code')
                     }
-        return {"created_at": result[0],
-                "status_code": result[1]
+                )
+    conn.close()
+    return result
+
+
+def get_all_checks(url_id):
+    with connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            result = []
+            query = sql.SQL(
+                "SELECT {id}, {status}, {h1}, {title}, {desc}, {date} FROM {table}"
+                "WHERE {value_3} = %s"
+                "ORDER BY {id} DESC;"
+            ).format(
+                id=sql.Identifier('id'),
+                status=sql.Identifier('status_code'),
+                h1=sql.Identifier('h1'),
+                title=sql.Identifier('title'),
+                desc=sql.Identifier('description'),
+                date=sql.Identifier('created_at'),
+                value_3=sql.Identifier('url_id'),
+                table=sql.Identifier('url_checks')
+            )
+            cur.execute(query, (url_id,))
+            array = cur.fetchall()
+            for value in array:
+                result.append({
+                    'id': value[0],
+                    'status_code': value[1],
+                    'h1': value[2],
+                    'title': value[3],
+                    'description': value[4],
+                    'created_at': value[5]
+                })
+    conn.close()
+    return result
+
+
+def get_last_check(value_id):
+    with connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            query = sql.SQL(
+                "SELECT {create_at}, {status_code} FROM {table}"
+                "WHERE {value_2} = %s "
+                "ORDER BY {value_3} DESC LIMIT 1;").format(
+                create_at=sql.Identifier('created_at'),
+                status_code=sql.Identifier('status_code'),
+                value_2=sql.Identifier('url_id'),
+                value_3=sql.Identifier('id'),
+                table=sql.Identifier('url_checks')
+            )
+            cur.execute(query, (value_id,))
+            result = cur.fetchone()
+    conn.close()
+    if result is None:
+        return {"created_at": "",
+                "status_code": ""
                 }
+    return {"created_at": result[0],
+            "status_code": result[1]
+            }
